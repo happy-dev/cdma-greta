0e33f85eda9e07ba0e0f51b9dddf570e153830f6
Pauline Khun <khun.pauline@gmail.com>
2016-11-24 17:46:46 +0100

    First commit: Integration Header

--- wp-content/themes/sage/front-page.php
+++ wp-content/themes/sage/front-page.php
@@ -1,52 +1,57 @@
 <?php while (have_posts()) : the_post(); ?>
 
 <!-- OPENING IMAGE/VIDEO -->
+<div class="container">
+    <h2>Formations à la une</h2>
+    <a href="">Voir toutes les formations</a>
 
-<h2>Formations à la une</h2>
-<a href="">Voir toutes les formations</a>
+    <div class="row">
+        <article class=".col-md-3">
+            <!-- THE QUERY -->
+            <?php $posts = get_field('formations_une');
+                if( $posts ): ?>
+                    <?php foreach( $posts as $post): ?>
+                        <?php setup_postdata($post); ?>
+                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <?php //the_excerpt(); ?>
+                    <?php endforeach; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+        </article>
+    </div><!-- row end -->
+</div><!-- container end -->
 
-<div class="row">
-	<!-- THE QUERY -->
-	<?php $posts = get_field('formations_une');
-		if( $posts ): ?>
-		    <?php foreach( $posts as $post): ?>
-		        <?php setup_postdata($post); ?>
-			        <div>
-			            <a href="<?php the_permalink(); ?>">
-			            	<?php the_title(); ?>
-			            </a>
-			            <?php //the_excerpt(); ?>
-			        </div>
-		    <?php endforeach; ?>
-		    <?php wp_reset_postdata(); ?>
-		<?php endif; ?>
-</div><!-- row end -->
+<div class="container">
+    <h2>Le Greta CDMA</h2>
+    <?php the_content(); ?>
+</div><!-- container end -->
 
-<h2>Le Greta CDMA</h2>
-<?php the_content(); ?>
+<div class="container">
+    <h2>Actualités</h2>
+    <a href="/actualites">Voir toute l'actualité</a>
 
-<h2>Actualités</h2>
-<a href="/actualites">Voir toute l'actualité</a>
+    <div class="row">
+        <!-- THE QUERY -->
+        <?php 
+        $args=( array( 	'post_type' => 'post' ,
+                        'category_name'  => 'a-la-une',
+                        'posts_per_page' => 6  ) );  
+        $the_query = new WP_Query( $args ); 
+            while ( $the_query->have_posts()) : $the_query->the_post(); ?>
+                <div>
+                    <a href= <?php the_permalink(); ?> >
+                        <?php the_title(); ?>
+                    </a>
+                    <?php //the_excerpt(); ?>
+                </div>	
+            <?php endwhile; ?>
+        <?php wp_reset_postdata();?>
+    </div><!-- row end -->
+</div><!-- container end -->
 
-<div class="row">
-	<!-- THE QUERY -->
-	<?php 
-	$args=( array( 	'post_type' => 'post' ,
-					'category_name'  => 'a-la-une',
-					'posts_per_page' => 6  ) );  
-	$the_query = new WP_Query( $args ); 
-		while ( $the_query->have_posts()) : $the_query->the_post(); ?>
-			<div>
-				<a href= <?php the_permalink(); ?> >
-					<?php the_title(); ?>
-				</a>
-				<?php //the_excerpt(); ?>
-			</div>	
-		<?php endwhile; ?>
-	<?php wp_reset_postdata();?>
-</div><!-- row end -->
-
-<h2>Lieux de formation</h2>
+<div class="container">
+    <h2>Lieux de formation</h2>
+</div><!-- container end -->
 
 
 
--- wp-content/themes/sage/templates/header.php
+++ wp-content/themes/sage/templates/header.php
@@ -1,12 +1,15 @@
-<header class="banner">
-  <div class="container">
+<header class="navbar">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" href="#collapse-nav-primary" aria-expanded="false" aria-controls="collapse-nav-primary">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <nav class="nav-primary collapse" id="collapse-nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
-  </div>
 </header>
