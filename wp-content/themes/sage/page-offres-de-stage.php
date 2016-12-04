27d903b83b6fb65b6e5c8bfe21b6db45d8a1272c
sylviakraf <sylviagraphisme@gmail.com>
2016-12-04 23:53:01 +0100

    offres de stage

--- wp-content/themes/sage/page-offres-de-stage.php
+++ wp-content/themes/sage/page-offres-de-stage.php
@@ -7,16 +7,10 @@
 	$the_query = new WP_Query( $args ); 
 		while ( $the_query->have_posts()) : $the_query->the_post(); ?>
 			<div>
-				<a href= <?php the_permalink(); ?> >
-					<?php the_title(); ?>
-				</a>
-				<?php the_field('commune_stage'); ?>
 				<div><?php the_field('nature_stage'); ?></div>
 				<div>Date de début : <?php the_field('debut_stage'); ?></div>
-				<div>Profil recherché : <?php the_field('profil_stage'); ?></div>
-				<div>Personne à contacter : <?php the_field('contact_stage'); ?></div>
-				<div>Email : <?php the_field('email_stage'); ?></div>
-				<div><?php the_excerpt(); ?></div>
+				<a href= <?php the_permalink(); ?> ><?php the_title(); ?></a> - <?php the_field('commune_stage'); ?>
+				<div><?php the_field('activite_stage'); ?></div>
 			</div>	
 		<?php endwhile; ?>
 	<?php wp_reset_postdata();?>
--- wp-content/themes/sage/single-stages.php
+++ wp-content/themes/sage/single-stages.php
@@ -1,12 +1,35 @@
 <?php while (have_posts()) : the_post(); ?>
+
 	
 <h1><?php the_title(); ?></h1>
-<?php the_field('commune_stage'); ?>
-<div><?php the_field('nature_stage'); ?></div>
-<div>Date de début : <?php the_field('debut_stage'); ?></div>
-<div>Profil recherché : <?php the_field('profil_stage'); ?></div>
-<div>Personne à contacter : <?php the_field('contact_stage'); ?></div>
+<h2><?php the_field('nature_stage'); ?> / <?php the_field('duree_stage'); ?></h2>
+<div><?php the_field('commune_stage'); ?></div>
+<div>Publié le <?php the_date(); ?></div>
+
+<br/>
+<br/>
+<h3>Informations sur la société</h3>
+<h4>Société ou organisme : <?php the_field('societe_stage'); ?></h4>
+<div>Site web : 
+	<a href="<?php the_field('site_stage'); ?>" target="_blank">
+		<?php the_field('site_stage'); ?>
+	</a>
+</div>
+<h4>Personne à contacter : <?php the_field('contact_stage'); ?></h4>
 <div>Email : <?php the_field('email_stage'); ?></div>
-<div><?php the_excerpt(); ?></div>					
+<div>Téléphone : <?php the_field('tel_stage'); ?></div>
+<div>Description : <?php the_field('text_societe_stage'); ?></div>
+
+<br/>
+<br/>
+<h3>Informations sur le poste</h3>
+<h4>Intitulé du poste : <?php the_title(); ?></h4>
+<div>Lieu : <?php the_field('lieu_stage'); ?></div>
+<h4>Date de début : <?php the_field('debut_stage'); ?></h4>
+<h4>Description de l'activité : </h4>
+<div><?php the_field('activite_stage'); ?></div>
+<h4>Profil souhaité : </h4>
+<div><?php the_field('profil_stage'); ?></div>
+					
 
 <?php endwhile; ?>
