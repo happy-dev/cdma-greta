28c441252ea06a09e0ae43e0dfd1e1b25754d9e9
sylviakraf <sylviagraphisme@gmail.com>
2017-02-17 18:15:38 +0100

    page header image

--- wp-content/themes/sage/single-stages.php
+++ wp-content/themes/sage/single-stages.php
@@ -22,13 +22,13 @@
 
 <h1><?php the_title(); ?></h1>
 <h2><?php the_field('nature_stage'); ?> / <?php the_field('duree_stage'); ?></h2>
-<div><?php the_field('commune_stage'); ?></div>
+<?php the_field('commune_stage'); ?>
 <div>Publié le <?php the_date(); ?></div>
 
 <br/>
 <br/>
 <h3>Informations sur la société</h3>
-<h4>Société ou organisme :</h4><div><?php the_field('societe_stage'); ?></div>
+<h4>Société ou organisme :</h4><?php the_field('societe_stage'); ?>
 <h4>Site web : </h4>
     <a href="<?php the_field('site_stage'); ?>" target="_blank">
         <?php the_field('site_stage'); ?>
@@ -46,10 +46,10 @@
 <div><h4>Intitulé du poste : </h4><?php the_title(); ?></div>
 <div><h4>Lieu : </h4><?php the_field('lieu_stage'); ?></div>
 <div><h4>Date de début : </h4><?php the_field('debut_stage'); ?></div>
-<h4>Description de l'activité : </h4>
-<div><?php the_field('activite_stage'); ?></div>
+Description de l'activité :
+<?php the_field('activite_stage'); ?>
 <h4>Profil souhaité : </h4>
-<div><?php the_field('profil_stage'); ?></div>
+<?php the_field('profil_stage'); ?>
 <h4>Date d'expiration de l'offre de stage : </h4>
 <div><?php the_field('exp_stage'); ?></div>
 
--- wp-content/themes/sage/templates/page-header.php
+++ wp-content/themes/sage/templates/page-header.php
@@ -1,15 +1,18 @@
 <?php use Roots\Sage\Titles; ?>
 
-<section class="presentation presentation-page">
+<?php
+    $imageArray = get_field('page_header_img');
+    if( !empty($imageArray) ): 
+    $image = $imageArray['url']; 
+    endif; ?> 
+
+<section class="presentation presentation-page" style="background-image: url(<?php echo $image; ?>); background-size:cover; ">
     <div class="container">
-        <figure>
-            <img class="image-background" src="http://127.0.0.1/~pauline/cdma/greta-cdma/wp-content/uploads/2016/12/page-intro.jpg" alt="">
-        </figure>
         <div class="row row-intro">
             <div class="col-md-3"></div>
             <div class="intro col-md-6 col-sm-12 col-xs-12">
                 <h1><?= Titles\title(); ?></h1>
-                <p>Le GRETA de la Création, du Design et Métiers d’Art est une structure de l’Éducation nationale qui propose des formations pour adultes. Ces formations sont organisées en cours du soir ou en journée pour répondre aux besoins en compétences des entreprises et des particuliers.</p>
+                <?php the_field('page_header_text') ?>
             </div>
             <div class="col-md-3"></div>
         </div>
