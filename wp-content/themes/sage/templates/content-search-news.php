2de64be9e98fb9aad5ad0e324edec068eaab945c
Alexandre Bourlier <alexandre@happy-dev.fr>
2017-05-23 10:00:57 +0000

    [bugfix] category in news search results

--- wp-content/themes/sage/templates/content-search-news.php
+++ wp-content/themes/sage/templates/content-search-news.php
@@ -12,7 +12,17 @@
             <?php endif; ?>
         <img style="width:100%;" src="<?php echo $thumb ?>" alt="<?php echo $alt; ?>" />
         <h3><?php the_title(); ?></h3>
-        <span><?php the_date() ?></span>
+        <span>
+	<?php 
+	  $cats = get_the_category();
+	  $arr  = [];
+	  foreach($cats as $cat) {
+	    $arr[] = $cat->name;
+	  }
+	  
+	  echo implode(', ', $arr);
+	?>
+	</span>
         <?php the_excerpt(); ?>
     </a>
 </article>
