03a5d621dc55863b22d04cadfd6a76dad837b913
Alex Bourlier <alex@paris.happy-dev.fr>
2022-04-03 15:59:13 +0200

    update: fixed broken link on front page

--- wp-content/themes/sage/templates/formations-a-la-une.php
+++ wp-content/themes/sage/templates/formations-a-la-une.php
@@ -1,6 +1,6 @@
 <section class="articles container">
   <h2>Formations à la une</h2>
-  <a class="see-all hidden-md-down" href="/se-former-au-greta">Voir toutes les formations</a>
+  <a class="see-all hidden-md-down" href="/domaine-offres">Voir toutes les formations</a>
 
   <div class="content row">
     <?= Dokelio::getHighlights(); ?>
