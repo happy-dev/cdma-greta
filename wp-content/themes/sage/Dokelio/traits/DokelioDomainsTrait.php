4a81a0d9f132f927abc0c5b3b146803406c6b39e
Alex Bourlier <alex@paris.happy-dev.fr>
2022-04-04 15:41:11 +0200

    update: update slub SQL field name to match export file

--- wp-content/themes/sage/Dokelio/traits/DokelioDomainsTrait.php
+++ wp-content/themes/sage/Dokelio/traits/DokelioDomainsTrait.php
@@ -16,7 +16,7 @@ Trait DokelioDomainsTrait {
   }
 
   public static function getDomain($slug) {
-    $query_string = "SELECT domaine_libelle FROM formation WHERE domaine_slug='". $slug ."' LIMIT 1";
+    $query_string = "SELECT domaine_libelle FROM formation WHERE slug='". $slug ."' LIMIT 1";
 
     if ($domain = Dokelio::$connection->query($query_string))
       return $domain->fetch_object()->domaine_libelle;
@@ -24,7 +24,7 @@ Trait DokelioDomainsTrait {
 
   public static function getFormations($slug=null) {
     $buffer = array();
-    $where_str = $slug ?  " WHERE domaine_slug='". $slug ."'" : "";
+    $where_str = $slug ?  " WHERE slug='". $slug ."'" : "";
     $query_string = "SELECT DISTINCT domaine_libelle, domaine_accroche, url_video_domaine, code_AF, flag_avant, synth_titre, synth_periode_de_formation, objectif_formation FROM formation". $where_str ." ORDER BY flag_avant DESC";
 
     if ($formations = Dokelio::$connection->query($query_string)) {
--- wp-content/themes/sage/dokelio-import-db.php
+++ wp-content/themes/sage/dokelio-import-db.php
@@ -4,12 +4,13 @@
 
   if (array_key_exists(1, $argv))// If filename passed as argument of the command line
     $file_name = $argv[1];
+  else 
+    shell_exec("scp cdma@securesftp.scola.ac-paris.fr:~/export_cdma_". date('Ymd') ."_*[0-9].txt ./dokelio_db_export_". date('Y-m-d') .".txt");
 
   $env = explode('/', __FILE__)[3];// Path to prod or staging environment
   include($env .'/wp-config.php');
   chdir($env .'/wp-content/themes/sage/dokelio_db_exports/');
 
-  shell_exec("scp cdma@securesftp.scola.ac-paris.fr:~/export_cdma_". date('Ymd') ."_*[0-9].txt ./dokelio_db_export_". date('Y-m-d') .".txt");
 
   if (!$file_name) 
     $file_name = 'dokelio_db_export_'. date('Y-m-d') .'.txt';
