0e9b8a333acc4d24f8a2895779f77332129e9a2e
Alex Bourlier <alex@paris.happy-dev.fr>
2022-04-06 23:06:55 +0200

    update: cleaned highlights code to separate view from controller

--- wp-content/themes/sage/Dokelio/traits/DokelioFrontpageTrait.php
+++ wp-content/themes/sage/Dokelio/traits/DokelioFrontpageTrait.php
@@ -1,18 +1,12 @@
 <?php
 Trait DokelioFrontpageTrait {
   public static function getHighlights() {
-    $buffer = '';
+    $buffer = array();
     $query_string = "SELECT DISTINCT code_AF, synth_titre, synth_periode_de_formation FROM formation WHERE flag_une = 1";
 
     if ($highlights = Dokelio::$connection->query($query_string)) {
       while($formation = $highlights->fetch_object()){
-	$buffer .= '<article class="entry col-md-4">';
-        $buffer .=   '<a href="https://cdma.happy-dev.fr/fiches/formation-wordpress-concevoir-un-site-vitrine/" title="'. $formation->synth_titre .'">';
-        $buffer .=     '<img src="https://cdma.happy-dev.fr/wp-content/uploads/Creer_un_site_wordpress-500x282.jpg" alt="" />';
-        $buffer .=     '<h3>'. $formation->synth_titre .'</h3>';
-        $buffer .=     '<span>'. $formation->synth_periode_de_formation .'</span>';
-        $buffer .=   '</a>';
-        $buffer .= '</article>';
+        $buffer[] = clone $formation;
       }
     }
     $highlights->close();
