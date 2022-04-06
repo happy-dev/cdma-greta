c7305dcfc3ad30e88343ccd7f5078a972bd3c6e0
Alex Bourlier <alex@paris.happy-dev.fr>
2022-04-06 23:08:25 +0200

    update: because we should

--- wp-content/themes/sage/templates/formations-a-la-une.php
+++ wp-content/themes/sage/templates/formations-a-la-une.php
@@ -3,20 +3,14 @@
   <a class="see-all hidden-md-down" href="/domaine-offres">Voir toutes les formations</a>
 
   <div class="content row">
-  <?php 
-    $formations = Dokelio::getHighlights();
-
-    foreach($formations as $formation) :
-  ?>
-      <article class="entry col-md-4">
-       <a href="https://cdma.happy-dev.fr/fiches/formation-wordpress-concevoir-un-site-vitrine/" title="'. $formation->synth_titre .'">
-         <img src="https://cdma.happy-dev.fr/wp-content/uploads/Creer_un_site_wordpress-500x282.jpg" alt="" />
-         <h3><?= $formation->synth_titre ?></h3>
-         <span><?= $formation->synth_periode_de_formation ?></span>
-       </a>
-      </article>
-  <?php
-    endforeach;
-  ?>
+  <?php foreach(Dokelio::getHighlights() as $formation) : ?>
+    <article class="entry col-md-4">
+     <a href="https://cdma.happy-dev.fr/fiches/formation-wordpress-concevoir-un-site-vitrine/" title="'. $formation->synth_titre .'">
+       <img src="https://cdma.happy-dev.fr/wp-content/uploads/Creer_un_site_wordpress-500x282.jpg" alt="" />
+       <h3><?= $formation->synth_titre ?></h3>
+       <span><?= $formation->synth_periode_de_formation ?></span>
+     </a>
+    </article>
+  <?php endforeach; ?>
   </div><!-- row end -->
 </section><!-- container end -->
