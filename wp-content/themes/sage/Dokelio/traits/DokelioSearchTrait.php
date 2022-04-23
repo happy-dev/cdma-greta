bbbb363b89f9c5a0dd0059ce65b1d670c82e2cf3
Alex Bourlier <alex@paris.happy-dev.fr>
2022-04-24 00:32:05 +0200

    update: improve search by allowing partial matches of words

--- wp-content/themes/sage/Dokelio/traits/DokelioSearchTrait.php
+++ wp-content/themes/sage/Dokelio/traits/DokelioSearchTrait.php
@@ -1,9 +1,9 @@
 <?php
 Trait DokelioSearchTrait {
   public static function search($str, $filter) {
-    $str = Dokelio::$connection->real_escape_string($str);
+    $str = '*'. str_replace(' ', '* *', Dokelio::$connection->real_escape_string($str)) .'*';
     $buffer = array();
-    $query_string = "SELECT code_AF, synth_titre, synth_periode_de_formation, synth_formation_accroche, MATCH (synth_titre) AGAINST ('$str' IN BOOLEAN MODE) AS titre, MATCH (synth_formation_accroche) AGAINST ('$str' IN BOOLEAN MODE) AS accroche, MATCH (contact) AGAINST ('$str' IN BOOLEAN MODE) AS coordo, MATCH (lieu_de_formation) AGAINST ('$str' IN BOOLEAN MODE) AS lieu, MATCH (domaine_libelle) AGAINST ('$str' IN BOOLEAN MODE) AS domaine FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, domaine_libelle) AGAINST ('$str' IN BOOLEAN MODE) $filter GROUP BY synth_titre ORDER BY (titre*4)+(accroche*2)+(coordo*1)+(lieu*1)+(domaine*1) DESC";
+    $query_string = "SELECT code_AF, synth_titre, synth_periode_de_formation, synth_formation_accroche, MATCH (synth_titre) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS titre, MATCH (synth_formation_accroche) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS accroche, MATCH (contact) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS coordo, MATCH (lieu_de_formation) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS lieu, MATCH (domaine_libelle) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS domaine FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, domaine_libelle) AGAINST ('$str' IN NATURAL LANGUAGE MODE) $filter GROUP BY synth_titre ORDER BY (titre*4)+(accroche*2)+(coordo*1)+(lieu*1)+(domaine*1) DESC";
 
     if ($formations = Dokelio::$connection->query($query_string)) {
       while($formation = $formations->fetch_object()) {
