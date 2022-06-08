6da8b1a9796f8e1a47dd519ce46973b528e6c712
Alex Bourlier <alex@paris.happy-dev.fr>
2022-06-08 15:45:15 +0200

    update: CRM API handling applications properly

--- wp-content/themes/sage/crm-api.php
+++ wp-content/themes/sage/crm-api.php
@@ -26,17 +26,67 @@ class CRM_API extends WP_REST_Controller {
    * @return WP_Error|WP_REST_Response
    **/
   public function get_applications( $request ) {
+    global $wpdb;
 
-    $items = array(); //do a query, call another class, etc
-    $data = array();
+    // We first retrieve the ID of the application form
+    $args = array(
+      'post_type' => 'wpcf7_contact_form',
+      'title' => 'Candidature',
+    );
+    $query = new WP_Query( $args );
 
-    foreach( $items as $item ) {
-      $itemdata = $this->prepare_item_for_response( $item, $request );
-      $data[] = $this->prepare_response_for_collection( $itemdata );
+    if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) {
+        $query->the_post();
+        $application_form_id = get_the_ID();
+      }
+    }
+
+    wp_reset_postdata();
+
+    $applications = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}db7_forms WHERE form_post_id={$application_form_id} AND form_date >= '2022-06-01 00:00:00' ORDER BY form_date DESC", OBJECT );
+    $applications_data = array();
+
+    foreach( $applications as $application ) {
+      $applications_data[] = $this->prepare_application_for_response( $application, $request );
     }
     
-    return new WP_REST_Response( 'Hello World!', 200 );
-    return new WP_REST_Response( $data, 200 );
+    return new WP_REST_Response( $applications_data, 200 );
+  }
+
+  /**
+   * Transform applications from DB to API format
+   * 
+   * @param Object $application Serialized object containing form's data
+   * @param WP_REST_Request $request Full data about the request.
+   * @return Array
+   **/
+  public function prepare_application_for_response( $application, $request ) {
+    $application_data = unserialize( $application->form_value );
+
+    if ($application_data) {
+      unset( $application_data['cfdb7_status'] );
+      unset( $application_data['privacy-agreement'] );
+      unset( $application_data['cf7sr-recaptcha'] );
+
+      $buffer = explode( ', ', $application_data['email-address'] );
+      $buffer = array_unique( array_map( 'trim', $buffer ) );
+      $application_data['email-address'] = implode( ', ', $buffer );
+
+      $application_data['formation-title'] = $application_data['candidate-formation'];
+      unset( $application_data['candidate-formation'] );
+
+      $application_data['gender'] = $application_data['gender'][0];
+      $application_data['candidate-diploma'] = $application_data['candidate-diploma'][0];
+      $application_data['candidate-handicapped'] = $application_data['candidate-handicapped'][0];
+      $application_data['candidate-jobsituation'] = $application_data['candidate-jobsituation'][0];
+      $application_data['candidate-demarche'] = $application_data['candidate-demarche'][0];
+      $application_data['info-greta'] = $application_data['info-greta'][0];
+      $application_data['application-date'] = $application->form_date;
+      $application_data['application-id'] = $application->form_id;
+    }
+
+    return $application_data;
   }
 
   /**
