2a02b1b2a415803e89dfe563eed29e1063500004
Alex Bourlier <alex@paris.happy-dev.fr>
2022-07-21 20:12:43 +0200

    update: search back to naural language mode

--- wp-content/themes/sage/Dokelio/traits/DokelioSearchTrait.php
+++ wp-content/themes/sage/Dokelio/traits/DokelioSearchTrait.php
@@ -41,8 +41,8 @@ Trait DokelioSearchTrait {
       }
       foreach ($str_arr as $idx => $word)
 	$str_arr[$idx] = dokelio::$connection->real_escape_string($word);
-      $str = '*'. implode('* *', $str_arr) .'*';
-      $query_string = "SELECT code_AF, synth_titre, slug_formation, synth_periode_de_formation, synth_formation_accroche, nom_image_formation, MATCH (synth_titre) AGAINST ('$str' IN BOOLEAN MODE) AS titre, MATCH (synth_formation_accroche) AGAINST ('$str' IN BOOLEAN MODE) AS accroche, MATCH (contact) AGAINST ('$str' IN BOOLEAN MODE) AS coordo, MATCH (lieu_de_formation) AGAINST ('$str' IN BOOLEAN MODE) AS lieu, MATCH (lib_domaine) AGAINST ('$str' IN BOOLEAN MODE) AS domaine FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN BOOLEAN MODE) $and $filter GROUP BY synth_titre ORDER BY (titre*4)+(accroche*2)+(coordo*1)+(lieu*1)+(domaine*1) DESC LIMIT ". CDMA_LIMIT ." $offset";
+      $str = implode(' ', $str_arr);
+      $query_string = "SELECT code_AF, synth_titre, slug_formation, synth_periode_de_formation, synth_formation_accroche, nom_image_formation, MATCH (synth_titre) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS titre, MATCH (synth_formation_accroche) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS accroche, MATCH (contact) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS coordo, MATCH (lieu_de_formation) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS lieu, MATCH (lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS domaine FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) $and $filter GROUP BY synth_titre ORDER BY (titre*8)+(accroche*2)+(coordo*1)+(lieu*1)+(domaine*1) DESC LIMIT ". CDMA_LIMIT ." $offset";
     }
 
     if ($formations = Dokelio::$connection->query($query_string)) {
@@ -84,8 +84,8 @@ Trait DokelioSearchTrait {
       $query_string = "SELECT COUNT(DISTINCT synth_titre) AS count FROM formation $where $filter";
     }
     else {
-      $str = '*'. str_replace(' ', '* *', Dokelio::$connection->real_escape_string($str)) .'*';
-      $query_string = "SELECT COUNT(DISTINCT synth_titre) AS count FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN BOOLEAN MODE) $and $filter LIMIT ". CDMA_LIMIT;
+      $str = Dokelio::$connection->real_escape_string($str);
+      $query_string = "SELECT COUNT(DISTINCT synth_titre) AS count FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) $and $filter LIMIT ". CDMA_LIMIT;
     }
 
     if ($counts = Dokelio::$connection->query($query_string)) {
