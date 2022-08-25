08de248b97e464da16a80602de7d6879f7db0399
Alex Bourlier <alex@paris.happy-dev.fr>
2022-08-25 06:14:21 +0200

    update: improvement trial of the search engine

--- wp-content/themes/sage/Dokelio/traits/DokelioSearchTrait.php
+++ wp-content/themes/sage/Dokelio/traits/DokelioSearchTrait.php
@@ -36,13 +36,13 @@ Trait DokelioSearchTrait {
     else {
       $str_arr = explode(' ', $str);
       foreach ($str_arr as $idx => $word) {// Remonving short words
-	if (strlen($word) <= 2)
+	if (strlen($word) <= 3)
 	  unset($str_arr[$idx]);
       }
       foreach ($str_arr as $idx => $word)
 	$str_arr[$idx] = dokelio::$connection->real_escape_string($word);
       $str = implode(' ', $str_arr);
-      $query_string = "SELECT code_AF, synth_titre, slug_formation, synth_periode_de_formation, synth_formation_accroche, nom_image_formation, MATCH (synth_titre) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS titre, MATCH (synth_formation_accroche) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS accroche, MATCH (contact) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS coordo, MATCH (lieu_de_formation) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS lieu, MATCH (lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS domaine FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) $and $filter GROUP BY synth_titre ORDER BY (titre*8)+(accroche*2)+(coordo*1)+(lieu*1)+(domaine*1) DESC LIMIT ". CDMA_LIMIT ." $offset";
+      $query_string = "SELECT code_AF, synth_titre, slug_formation, synth_periode_de_formation, synth_formation_accroche, nom_image_formation, MATCH (synth_titre) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS titre, MATCH (synth_formation_accroche) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS accroche, MATCH (contact) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS coordo, MATCH (lieu_de_formation) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS lieu, MATCH (lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) AS domaine FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) $and $filter GROUP BY synth_titre ORDER BY titre*4 + accroche DESC LIMIT ". CDMA_LIMIT ." $offset";
     }
 
     if ($formations = Dokelio::$connection->query($query_string)) {
@@ -84,7 +84,14 @@ Trait DokelioSearchTrait {
       $query_string = "SELECT COUNT(DISTINCT synth_titre) AS count FROM formation $where $filter";
     }
     else {
-      $str = Dokelio::$connection->real_escape_string($str);
+      $str_arr = explode(' ', $str);
+      foreach ($str_arr as $idx => $word) {// Remonving short words
+	if (strlen($word) <= 3)
+	  unset($str_arr[$idx]);
+      }
+      foreach ($str_arr as $idx => $word)
+	$str_arr[$idx] = dokelio::$connection->real_escape_string($word);
+      $str = implode(' ', $str_arr);
       $query_string = "SELECT COUNT(DISTINCT synth_titre) AS count FROM formation WHERE MATCH (synth_titre, synth_formation_accroche, contact, lieu_de_formation, lib_domaine) AGAINST ('$str' IN NATURAL LANGUAGE MODE) $and $filter LIMIT ". CDMA_LIMIT;
     }
 
