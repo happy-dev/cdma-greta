e7f8c3a582343e7e3d709f4673ae6a2d86228c71
Alex Bourlier <alex@paris.happy-dev.fr>
2023-01-25 19:04:35 +0100

    api: adding route for info requests

--- wp-content/themes/sage/crm-api.php
+++ wp-content/themes/sage/crm-api.php
@@ -7,16 +7,25 @@ class CRM_API extends WP_REST_Controller {
   public function register_routes() {
     $version = '1';
     $namespace = 'cdma-crm-api/v' . $version;
-    $base = 'applications';
 
-    register_rest_route( $namespace, '/' . $base, array(
+    register_rest_route( $namespace, '/applications', array(
       array(
         'methods'             => WP_REST_Server::READABLE,
         'callback'            => array( $this, 'get_applications' ),
-        'permission_callback' => array( $this, 'get_applications_permissions_check' ),
+        'permission_callback' => array( $this, 'crm_api_permissions_check' ),
         'args'                => array(),
       ),
     ));
+
+    register_rest_route( $namespace, '/info-requests', array(
+      array(
+        'methods'             => WP_REST_Server::READABLE,
+        'callback'            => array( $this, 'get_info_requests' ),
+        'permission_callback' => array( $this, 'crm_api_permissions_check' ),
+        'args'                => array(),
+      ),
+    ));
+
   }
 
   /**
@@ -55,6 +64,41 @@ class CRM_API extends WP_REST_Controller {
   }
 
   /**
+   * Get the information requests to the different formations
+   * 
+   * @param WP_REST_Request $request Full data about the request.
+   * @return WP_Error|WP_REST_Response
+   **/
+  public function get_info_requests( $request ) {
+    global $wpdb;
+
+    // We first retrieve the ID of the info requests form
+    $args = array(
+      'post_type' => 'wpcf7_contact_form',
+      'title' => 'Contact from formation',
+    );
+    $query = new WP_Query( $args );
+
+    if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) {
+        $query->the_post();
+        $info_request_form_id = get_the_ID();
+      }
+    }
+
+    wp_reset_postdata();
+
+    $info_requests = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}db7_forms WHERE form_post_id={$info_request_form_id} AND form_date >= '2022-06-01 00:00:00' ORDER BY form_date DESC", OBJECT );
+    $info_requests_data = array();
+
+    foreach( $info_requests as $info_request ) {
+      $info_requests_data[] = $this->prepare_info_request_for_response( $info_request, $request );
+    }
+    
+    return new WP_REST_Response( $info_requests_data, 200 );
+  }
+
+  /**
    * Transform applications from DB to API format
    * 
    * @param Object $application Serialized object containing form's data
@@ -72,7 +116,9 @@ class CRM_API extends WP_REST_Controller {
       $application_data['formation-title'] = $application_data['candidate-formation'];
       unset( $application_data['candidate-formation'] );
 
-      $application_data['gender'] = $application_data['gender'][0];
+      $application_data['candidate-gender'] = $application_data['gender'][0];
+      unset( $application_data['gender'] );
+
       $application_data['candidate-diploma'] = $application_data['candidate-diploma'][0];
       $application_data['candidate-handicapped'] = $application_data['candidate-handicapped'][0];
       $application_data['candidate-jobsituation'] = $application_data['candidate-jobsituation'][0];
@@ -82,7 +128,37 @@ class CRM_API extends WP_REST_Controller {
       $application_data['application-id'] = $application->form_id;
     }
 
+    ksort($application_data);
     return $application_data;
+  }
+
+  /**
+   * Transform info requests from DB to API format
+   * 
+   * @param Object $info_request Serialized object containing form's data
+   * @param WP_REST_Request $request Full data about the request.
+   * @return Array
+   **/
+  public function prepare_info_request_for_response( $info_request, $request ) {
+    $info_request_data = unserialize( $info_request->form_value );
+
+    if ($info_request_data) {
+      unset( $info_request_data['cfdb7_status'] );
+      unset( $info_request_data['privacy-agreement'] );
+      unset( $info_request_data['cf7sr-recaptcha'] );
+
+      $info_request_data['formation-title'] = $info_request_data['formation'];
+      unset( $info_request_data['formation'] );
+
+      $info_request_data['candidate-gender'] = $info_request_data['gender'][0];
+      unset( $info_request_data['gender'] );
+
+      $info_request_data['application-date'] = $info_request->form_date;
+      $info_request_data['application-id'] = $info_request->form_id;
+    }
+
+    ksort($info_request_data);
+    return $info_request_data;
   }
 
   /**
@@ -91,7 +167,7 @@ class CRM_API extends WP_REST_Controller {
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|bool
    **/
-  public function get_applications_permissions_check( $request ) {
+  public function crm_api_permissions_check( $request ) {
     return current_user_can( 'read_crm_api' );
   }
 }
